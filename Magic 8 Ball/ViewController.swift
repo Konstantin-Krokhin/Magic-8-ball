9e64cfdd8521d202be9dbee11957e58deb174b7d
Konstantin-Krokhin <winpie1@gmail.com>
2023-02-09 14:48:17 +0100

    Finished Magic 8 Ball application

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var gifImageView: UIImageView!
+    let ballArray = [UIImage(imageLiteralResourceName: "ball1"), UIImage(imageLiteralResourceName: "ball2"), UIImage(imageLiteralResourceName: "ball3"), UIImage(imageLiteralResourceName: "ball4"), UIImage(imageLiteralResourceName: "ball5")];
 
-
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "cosmos.jpeg")!)
+        let imageGif = UIImage.gifImageWithName("nah-8ball")
+        gifImageView.image = imageGif
+    }
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        imageView.image = ballArray[Int.random(in: 0...4)]
+    }
+//
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            imageView.image = ballArray[Int.random(in: 0...4)]
+        }
+    }
+    
 }
 
